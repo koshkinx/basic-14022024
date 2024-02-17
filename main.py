ee13cd913468d8f3463713b1223a20bec1369062
Dmytro Senchuk <koshkinx9@gmail.com>
2024-02-17 21:30:44 +0200

    lesson2

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello world')
+
+number = int(input("enter a 4-digit number: "))
+
+print(number // 1000)
+print(number % 1000 // 100)
+print(number % 100 // 10)
+print(number % 10 // 1)
+
+
+
+number = int(input("enter a 5-digit number: "))
+
+reversed_number = (number % 10) * 10000 + ((number // 10) % 10) * 1000 + ((number // 100) % 10) * 100 + ((number // 1000) % 10) * 10 + (number // 10000)
+
+print("result:", reversed_number)
