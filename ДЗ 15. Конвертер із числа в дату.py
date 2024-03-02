c8982655b954d6e964f7ef96a6c16ae2f63ea93a
Dmytro Senchuk <koshkinx9@gmail.com>
2024-03-02 22:42:08 +0200

    15

--- ДЗ 15. Конвертер із числа в дату.py
+++ ДЗ 15. Конвертер із числа в дату.py
@@ -1,9 +1,9 @@
 total_seconds = int(input("Введіть кількість секунд: "))
 
-days, remaining_seconds = divmod(total_seconds, 86400)
-hours, remaining_seconds = divmod(remaining_seconds, 3600)
-minutes, seconds = divmod(remaining_seconds, 60)
-
+days = total_seconds // 86400
+hours = (total_seconds % 86400) // 3600
+minutes = (total_seconds % 3600) // 60
+seconds = total_seconds % 60
 
 days_str = "день"
 if days == 1:
