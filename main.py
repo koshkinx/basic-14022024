4f750ae9834a3e6f006dcaab3cd7f01923bfe899
Dmytro Senchuk <koshkinx9@gmail.com>
2024-03-17 16:22:07 +0200

    22

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-def popular_words(text, words):
-    # Приводим текст к нижнему регистру
-    text_lower = text.lower()
-    # Разбиваем текст на слова
-    text_words = text_lower.split()
-    # Создаем словарь для подсчета популярности слов
-    word_count = {word: 0 for word in words}
-    # Подсчитываем количество вхождений каждого слова из списка в тексте
-    for word in text_words:
-        if word in word_count:
-            word_count[word] += 1
-    return word_count
+def difference(*args):
+    if not args:
+        return 0
+    else:
+        return abs(max(args) - min(args))
 
-# Проверка
-assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
 print('OK')
+print(difference(10.2, -2.2, 0, 1.1, 0.5))
