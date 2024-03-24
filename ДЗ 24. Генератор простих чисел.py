a72e322260d0c3f8ffa6b835920a68189ca88172
Dmytro Senchuk <koshkinx9@gmail.com>
2024-03-24 15:34:27 +0200

    24

--- ДЗ 24. Генератор простих чисел.py
+++ ДЗ 24. Генератор простих чисел.py
@@ -1,6 +1,6 @@
 def prime_generator(end):
     sieve = [True] * (end + 1)
-    sieve[0:2] = [False, False] 
+    sieve[0:2] = [False, False]
 
     for i in range(2, int(end ** 0.5) + 1):
         if sieve[i]:
@@ -10,7 +10,6 @@ def prime_generator(end):
     for i in range(2, end + 1):
         if sieve[i]:
             yield i
-        
 
 
 from inspect import isgenerator
@@ -21,4 +20,4 @@ assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
 assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
 assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
 print('Ok')
-print(list(prime_generator(20)))
+print(list(prime_generator(10)))
